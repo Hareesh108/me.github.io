ac3f41629cbd81a28012aa3c41c1cd8fca654433
hareesh <hareeshbhittam@gmail.com>
2024-10-27 21:50:07 +0530

    Added navabar skeleton

---
+++ sections/navbar/index.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+// next and react
+import Link from "next/link";
+import React from "react";
+
+// components
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+// contexts
+import { useWindowEvent } from "@/contexts/window-event-provider";
+
+// utils
+import { cn } from "@/lib/utils";
+
+const Navbar = () => {
+  const { scrollY } = useWindowEvent();
+
+  const mainLink = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About Me",
+      link: "/about-me",
+    },
+    {
+      name: "Projects",
+      link: "/projects",
+    },
+    {
+      name: "Skills",
+      link: "/skills",
+    },
+    {
+      name: "Services",
+      link: "/services",
+    },
+    {
+      name: "Blog",
+      link: "/blog",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+
+  const stickToTop = scrollY > 50;
+
+  return (
+    <div
+      className={cn(
+        `flex justify-between items-center rounded-xl fixed z-[40] bg-opacity-80 py-4`,
+        "transition-all duration-300 ease-out",
+        stickToTop
+          ? "xs:top-4 xs:right-4 xs:left-4 xl:top-6  xl:right-6 xl:left-6 drop-shadow-lg pr-2"
+          : "w-full top-8 xs:pl-4 xs:pr-5 xl:pr-14",
+        stickToTop ? "bg-accent-light-100 bg-[#F8F9FB] " : undefined
+      )}
+    >
+      <NavigationMenu>
+        <NavigationMenuList>
+          {mainLink.map(({ name, link }) => (
+            <NavigationMenuItem key={name + link}>
+              <Link href={link} legacyBehavior passHref>
+                <NavigationMenuLink
+                  className={cn(
+                    navigationMenuTriggerStyle(undefined),
+                    "xs:hidden  text-[1.2em]",
+                    "lg:block text-[1.2em]"
+                  )}
+                >
+                  {name}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1  rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
+
+export default Navbar;

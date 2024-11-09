5566f652fcf9fd3e91d5cd4a59fc4a298612e305
hareesh <hareeshbhittam@gmail.com>
2024-11-09 17:33:01 +0530

    Handled work section

--- components/work/index.tsx
+++ components/work/index.tsx
@@ -90,113 +90,94 @@ export default function WorkPage() {
           </svg>
         </div>
 
-        <div className="w-full max-w-xl space-y-8 px-4">
-          <div className="text-center">
-            <h3 className="text-2xl font-bold text-primary">
-              Full Stack Developer | Banking Domain
-            </h3>
-            <p className="underline text-lg">Experience: 2+ years</p>
-          </div>
-
-          <ul className="space-y-6 list-disc list-inside text-justify justify-evenly">
-            <li>
+        <div className="w-full space-y-8 px-4 md:px-0">
+          <div className="grid md:grid-cols-2 gap-6 text-justify">
+            <div>
               <strong className="underline">
                 Developed and maintained a Back Office Portal for Bank Employees
               </strong>
-              <ul className="pl-6 list-disc list-inside text-justify">
-                <li>
-                  Built a secure, high-performance portal to streamline
-                  administrative tasks.
-                </li>
-                <li>
-                  Integrated role-based access control (RBAC) for secure data
-                  handling.
-                </li>
-              </ul>
-            </li>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Built a secure, high-performance portal to streamline
+                administrative tasks.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Integrated role-based access control (RBAC) for secure data
+                handling.
+              </p>
+            </div>
 
-            <li>
+            <div>
               <strong className="underline">
                 Frontend Development with Modern Web Technologies
               </strong>
-              <ul className="pl-6 list-disc list-inside text-justify">
-                <li>
-                  Utilized{" "}
-                  <span className="font-semibold text-classic">
-                    React, Next.js{" "}
-                  </span>
-                  and{" "}
-                  <span className="font-semibold text-classic">TypeScript</span>{" "}
-                  for responsive, user-friendly interfaces.
-                </li>
-                <li>
-                  Implemented{" "}
-                  <span className="font-semibold text-classic">
-                    Redux, React-hook-form, Yup, Zod, Chakra UI, Framer motion
-                  </span>{" "}
-                  and{" "}
-                  <span className="font-semibold text-classic">
-                    Material-UI
-                  </span>{" "}
-                  for consistent, accessible UI components.
-                </li>
-                <li>
-                  Managed state efficiently and optimized API data handling.
-                </li>
-              </ul>
-            </li>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Utilized{" "}
+                <span className="font-semibold text-classic">
+                  React, Next.js
+                </span>{" "}
+                and{" "}
+                <span className="font-semibold text-classic">TypeScript</span>{" "}
+                for responsive, user-friendly interfaces.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Implemented{" "}
+                <span className="font-semibold text-classic">
+                  Redux, React-hook-form, Yup, Zod, Chakra UI, Framer motion
+                </span>{" "}
+                and{" "}
+                <span className="font-semibold text-classic">Material-UI</span>{" "}
+                for consistent, accessible UI components.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Managed state efficiently and optimized API data handling.
+              </p>
+            </div>
 
-            <li>
+            <div>
               <strong className="underline">
                 Backend Development Using Java (Spring Boot) and Node.js
               </strong>
-              <ul className="pl-6 list-disc list-inside text-justify">
-                <li>
-                  Developed scalable REST APIs with{" "}
-                  <span className="font-semibold text-classic">
-                    Spring Boot
-                  </span>{" "}
-                  to handle high-traffic operations.
-                </li>
-                <li>
-                  Implemented data security with{" "}
-                  <span className="font-semibold text-classic">
-                    Keycloak [Identity and Access Management (IAM)]
-                  </span>{" "}
-                  and JWT authentication.
-                </li>
-                <li>
-                  Integrated{" "}
-                  <span className="font-semibold text-classic">PostgreSQL</span>{" "}
-                  as the primary relational database for data storage.
-                </li>
-                <li>
-                  Utilized{" "}
-                  <span className="font-semibold text-classic">Redis</span> for
-                  caching to improve performance and reduce database load.
-                </li>
-                <li>
-                  Enhanced data processing for real-time banking operations.
-                </li>
-              </ul>
-            </li>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Developed scalable REST APIs with{" "}
+                <span className="font-semibold text-classic">Spring Boot</span>{" "}
+                to handle high-traffic operations.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Implemented data security with{" "}
+                <span className="font-semibold text-classic">
+                  Keycloak [Identity and Access Management (IAM)]
+                </span>{" "}
+                and JWT authentication.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Integrated{" "}
+                <span className="font-semibold text-classic">PostgreSQL</span>{" "}
+                as the primary relational database for data storage.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Utilized{" "}
+                <span className="font-semibold text-classic">Redis</span> for
+                caching to improve performance and reduce database load.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Enhanced data processing for real-time banking operations.
+              </p>
+            </div>
 
-            <li>
+            <div>
               <strong className="underline">
                 Collaborated with Cross-functional Teams
               </strong>
-              <ul className="pl-6 list-disc list-inside text-justify">
-                <li>
-                  Worked closely with product managers, designers, and QA for
-                  quality assurance.
-                </li>
-                <li className="justify-evenly">
-                  Conducted code reviews, contributed to documentation, and
-                  ensured seamless project handover.
-                </li>
-              </ul>
-            </li>
-          </ul>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Worked closely with product managers, designers, and QA for
+                quality assurance.
+              </p>
+              <p className="mt-2 pl-4 relative before:content-['•'] before:absolute before:left-0">
+                Conducted code reviews, contributed to documentation, and
+                ensured seamless project handover.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
